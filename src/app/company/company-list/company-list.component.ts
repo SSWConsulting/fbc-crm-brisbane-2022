cd166b568983d79bebc7f5d1d6c519e2580b1fe6
Luke Cook <lukecook@ssw.com.au>
2022-05-23 14:42:35 +1000

    finished part 7 - rxjs http

--- src/app/company/company-list/company-list.component.ts
+++ src/app/company/company-list/company-list.component.ts
@@ -1,24 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { tap, map, Observable, finalize } from 'rxjs';
+
 import { Company } from '../company';
 import { CompanyService } from '../company.service';
 
 @Component({
   selector: 'fbc-company-list',
   templateUrl: './company-list.component.html',
-  styleUrls: ['./company-list.component.scss']
+  styleUrls: ['./company-list.component.scss'],
 })
 export class CompanyListComponent implements OnInit {
-
-  companies: Company[];
-  constructor(private companyService: CompanyService) {
-  }
+  companies$: Observable<Company[]>;
+  constructor(private companyService: CompanyService) {}
 
   ngOnInit(): void {
-    this.companies = this.getCompanies();
+    this.getCompanies();
   }
 
-  getCompanies(): Company[] {
-    return this.companyService.getCompanies();
+  deleteCompany(companyId: number) {
+    console.log('component.deleteCompany fired. companyId=', companyId);
+    this.companyService.deleteCompany(companyId)
+    .pipe(
+      finalize(() => console.log('Subscription completed'))
+    )
+    .subscribe( // Subscribe to the observable so it actually does something.
+      () => { this.getCompanies() } // do something (ie refresh our company list)
+    );
   }
 
+  getCompanies() {
+    this.companies$ = this.companyService.getCompanies().pipe(
+      tap((companies) => {
+        console.log('logging from Component ', companies);
+      }),
+      map((companies) => {
+        return companies;
+      })
+    );
+  }
 }
--- src/app/company/company.service.ts
+++ src/app/company/company.service.ts
@@ -1,18 +1,39 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError, tap, finalize } from 'rxjs/operators';
+
 import { Company } from './company';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CompanyService {
+  API_BASE = 'https://firebootcamp-crm-api.azurewebsites.net/api';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
 
-  getCompanies(): Company[] {
-    return [
-      { name: 'Jean Company', email: 'jean@acme.com', phone: 111 },
-      { name: 'Luke Company', email: 'luke@acme.com', phone: 222 },
-      { name: 'Matt Company', email: 'matt@acme.com', phone: 333 },
-    ];
+  getCompanies(): Observable<Company[]> {
+    return this.httpClient.get<Company[]>(`${this.API_BASE}/company`).pipe(
+      tap((companies) => {
+        console.log('logging from service ', companies);
+      }),
+      catchError((e) => this.errorHandler<Company[]>(e)),
+      finalize(() => {
+        console.log('finalized the stream');
+      })
+    );
+  }
+
+  deleteCompany(companyId): Observable<Company> {
+    console.log('service.deleteCompany fired. CompanyId=', companyId);
+    return this.httpClient
+      .delete<Company>(`${this.API_BASE}/company/${companyId}`)
+      .pipe(catchError((e) => this.errorHandler<Company>(e)));
+  }
+
+  private errorHandler<T>(error: Error): Observable<T> {
+    console.error('Error in CompanyService!');
+    return new Observable<T>();
   }
 }
