8f7883ed62e3d8d83dab8523126a7d20285c637f
JeanThirion <jean.thirion@esial.net>
2022-05-23 10:59:48 +1000

    initial commit adding FormsModule ✨

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'firebootcamp-crm-brisbane-2022';
+  title = 'Brisbane is awesome';
+
 }
+
